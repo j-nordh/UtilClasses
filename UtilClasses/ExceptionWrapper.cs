562c60b7bd6038f124c2236895305e614d9a5eb1
Johannes Nordh <jn@wideco.se>
2024-03-10 12:11:10 +0100

    Recreated ExceptionWrapper

---
+++ UtilClasses/ExceptionWrapper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace UtilClasses
+{
+    public class ExceptionWrapper
+    {
+        public string Title { get; set; }
+        public string StackTrace { get; set; }
+        public ExceptionWrapper InnerException { get; set; }
+
+        public static ExceptionWrapper From(Exception e)=>null == e
+        ? null
+        : new ()
+        {
+            Title = e.Message,
+            StackTrace = e.StackTrace,
+            InnerException = From(e)
+        };
+
+        public Exception Unwrap() => new Exception(Title, InnerException?.Unwrap());
+}
+    /*    private ExceptionWrapper GetException(Exception e) => null == e
+        ? null
+        : new()
+        {
+            Title = e.Message,
+            StackTrace = e.StackTrace,
+            InnerException = GetException(e.InnerException)
+        };*/
+}
