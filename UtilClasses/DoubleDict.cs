6d57787baa2f14779e399a9778603edf1ca14baa
Johannes Nordh <jn@wideco.se>
2024-03-10 12:07:31 +0100

    DoubleDict refactored and extracted an interface

--- UtilClasses/DoubleDict.cs
+++ UtilClasses/DoubleDict.cs
@@ -2,29 +2,23 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using UtilClasses.Extensions.Dictionaries;
 
 namespace UtilClasses;
 
-public class DoubleDict<T1, T2> where T1 : IComparable
+public class DoubleDict<T1, T2> : IDoubleDict<T1, T2> where T1 : IComparable
 {
     private Dictionary<T1, T2> _forward;
     private Dictionary<T2, T1> _reverse;
 
-    public List<(T1 a, T2 b)> State
-    {
-        get => _forward.Select(kvp => (kvp.Key, kvp.Value)).ToList();
-        set
-        {
-            _forward.Clear();
-            _reverse.Clear();
-            value.ForEach(x => Insert(x.a, x.b));
-        }
-    }
+    public List<(T1 a, T2 b)> State => _forward.Select(kvp => (kvp.Key, kvp.Value)).ToList();
 
-        
-    public DoubleDict(IEqualityComparer<T1>? cmpA, IEqualityComparer<T2>? cmpB)
+    public void Clear()
+    {
+        _forward.Clear();
+        _reverse.Clear();
+    }
+    public DoubleDict(IEqualityComparer<T1>? cmpA=null, IEqualityComparer<T2>? cmpB=null)
     {
         _forward = cmpA == null
             ? new Dictionary<T1, T2>()
@@ -33,19 +27,6 @@ public class DoubleDict<T1, T2> where T1 : IComparable
             ? new Dictionary<T2, T1>()
             : new Dictionary<T2, T1>(cmpB);
     }
-
-    public DoubleDict(IEqualityComparer<T1>? cmpA) : this(cmpA, null)
-    {
-    }
-
-    public DoubleDict(IEqualityComparer<T2>? cmpB) : this(null, cmpB)
-    {
-    }
-
-    public DoubleDict() : this(null, null)
-    {
-    }
-
     public DoubleDict(IEnumerable<(T1, T2)> items, IEqualityComparer<T1>? cmpA = null,
         IEqualityComparer<T2>? cmpB = null) : this(cmpA, cmpB)
     {
@@ -55,24 +36,34 @@ public class DoubleDict<T1, T2> where T1 : IComparable
             _reverse[b] = a;
         }
     }
-
-    public T1 GetOrAdd(T2 val, T1 newValue) => _reverse.GetOrAdd(val, newValue);
-    public T1 GetOrAdd(T2 val, Func<T1> f) => _reverse.GetOrAdd(val, f);
-    public T2 GetOrAdd(T1 val, T2 newValue) => _forward.GetOrAdd(val, newValue);
-    public T2 GetOrAdd(T1 val, Func<T2> f) => _forward.GetOrAdd(val, f);
-    public T2 Get(T1 val) => _forward[val];
-    public T1 Get(T2 val) => _reverse[val];
-
+    public T1 this[T2 v] => _reverse[v];
+    public T2 this[T1 v] => _forward[v];
     public (T1 a, T2 b) Insert(T1 a, T2 b)
     {
         _forward[a] = b;
         _reverse[b] = a;
         return (a, b);
     }
+    public T1 GetOrAdd(T2 val, Func<T1> f) => _reverse.GetOrAdd(val, f);
+    public T2 GetOrAdd(T1 val, Func<T2> f) => _forward.GetOrAdd(val, f);
+    public bool TryGetValue(T1 val, out T2 ret ) => _forward.TryGetValue(val, out ret);
+    public bool TryGetValue(T2 val, out T1 ret ) => _reverse.TryGetValue(val, out ret);
+    
+}
+
+public static class DoubleDictExtensions
+{
+    public static T1 GetOrAdd<T1, T2>(this IDoubleDict<T1, T2> dd, T2 val, T1 newValue) where T1:IComparable
+        => dd.GetOrAdd(val, ()=>newValue);
+    public static T2 GetOrAdd<T1, T2>(this IDoubleDict<T1, T2> dd, T1 val, T2 newValue) where T1:IComparable
+        => dd.GetOrAdd(val, ()=>newValue);
+    public static  T1 Get<T1, T2>(this IDoubleDict<T1, T2> dd, T2 val) where T1:IComparable => dd[val];
+    public static  T2 Get<T1, T2>(this IDoubleDict<T1, T2> dd, T1 val) where T1:IComparable => dd[val];
 }
 
 public static class DoubleDict
 {
+    
     public static DoubleDict<string, T2> Oic<T2>() => new(StringComparer.OrdinalIgnoreCase);
     public static DoubleDict<string, string> OicOic() => new(StringComparer.OrdinalIgnoreCase, StringComparer.OrdinalIgnoreCase);
 }
---
+++ UtilClasses/IDoubleDict.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UtilClasses;
+
+public interface IDoubleDict<T1, T2> where T1 : IComparable
+{
+    List<(T1 a, T2 b)> State { get; }
+    void Clear();
+    T1 this[T2 v] { get; }
+    T2 this[T1 v] { get; }
+    (T1 a, T2 b) Insert(T1 a, T2 b);
+    T1 GetOrAdd(T2 val, Func<T1> f);
+    T2 GetOrAdd(T1 val, Func<T2> f);
+    bool TryGetValue(T1 val, out T2 ret );
+    bool TryGetValue(T2 val, out T1 ret );
+}
