60eb49ce593c799e10472bb2d5b3d9ac3eae73cf
Johannes Nordh <jn@wideco.se>
2024-03-15 12:15:13 +0100

    JSON handling updated, removed old references

---
+++ UtilClasses.Json/BaseClassConverterSetter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Newtonsoft.Json;
+using UtilClasses.Json.Extensions;
+
+namespace UtilClasses.Json;
+
+public class BaseClassConverterSetter<T, TKey> : IJsonSettingSetter where TKey : struct, Enum, IConvertible, IComparable
+{
+    private BaseClassConverter<T, TKey> _conv;
+    public BaseClassConverterSetter()
+    {
+        _conv = new BaseClassConverter<T, TKey>();
+    }
+    public void Apply(JsonSerializerSettings settings)
+    {
+        settings.WithConverter(_conv);
+    }
+    public BaseClassConverterSetter<T, TKey> With<T2>(TKey k) where T2 : T, new()
+    {
+        _conv.With<T2>(k);
+        return this;
+    }
+}
---
+++ UtilClasses.Json/DefaultSetter.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace UtilClasses.Json;
+
+public class DefaultSetter : IJsonSettingSetter
+{
+    void IJsonSettingSetter.Apply(JsonSerializerSettings settings)
+    {
+        settings.Converters.Add(new StringEnumConverter());
+        settings.Formatting = Formatting.Indented;
+        settings.ObjectCreationHandling = ObjectCreationHandling.Replace;
+    }
+}
