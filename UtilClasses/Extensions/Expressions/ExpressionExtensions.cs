2390cc1ab498c583a9a7937025dc97f2fae92d81
Johannes Nordh <jn@wideco.se>
2024-03-26 10:46:33 +0100

    Added expression tree creation helpers

--- UtilClasses/Extensions/Expressions/ExpressionExtensions.cs
+++ UtilClasses/Extensions/Expressions/ExpressionExtensions.cs
@@ -31,6 +31,60 @@ namespace UtilClasses.Extensions.Expressions
                     throw new Exception("Could not generate a suitable accessor");
             }
         }
+        
+        public static Action<TEntity, TProperty> CreateSetter<TEntity, TProperty>(Expression<Func<TEntity, TProperty>> property)
+        {
+            PropertyInfo propertyInfo = GetProperty(property);
+
+            ParameterExpression instance = Expression.Parameter(typeof(TEntity), "instance");
+            ParameterExpression parameter = Expression.Parameter(typeof(TProperty), "param");
+
+            var body = Expression.Call(instance, propertyInfo.GetSetMethod(), parameter);
+            var parameters = new ParameterExpression[] { instance, parameter };
+
+            return Expression.Lambda<Action<TEntity, TProperty>>(body, parameters).Compile();
+        }
+        public static PropertyInfo GetProperty<TEntity, TProperty>(Expression<Func<TEntity, TProperty>> expression)
+        {
+            var member = GetMemberExpression(expression).Member;
+            var property = member as PropertyInfo;
+            if (property == null)
+            {
+                throw new InvalidOperationException(string.Format("Member with Name '{0}' is not a property.", member.Name));
+            }
+            return property;
+        }
+        private static MemberExpression GetMemberExpression<TEntity, TProperty>(Expression<Func<TEntity, TProperty>> expression)
+        {
+            MemberExpression memberExpression = null;
+            if (expression.Body.NodeType == ExpressionType.Convert)
+            {
+                var body = (UnaryExpression)expression.Body;
+                memberExpression = body.Operand as MemberExpression;
+            }
+            else if (expression.Body.NodeType == ExpressionType.MemberAccess)
+            {
+                memberExpression = expression.Body as MemberExpression;
+            }
+
+            if (memberExpression == null)
+            {
+                throw new ArgumentException("Not a member access", "expression");
+            }
+
+            return memberExpression;
+        }
+        public static Func<TEntity, TProperty> CreateGetter<TEntity, TProperty>(Expression<Func<TEntity, TProperty>> property)
+        {
+            PropertyInfo propertyInfo = GetProperty(property);
+
+            ParameterExpression instance = Expression.Parameter(typeof(TEntity), "instance");
+
+            var body = Expression.Call(instance, propertyInfo.GetGetMethod());
+            var parameters = new ParameterExpression[] { instance };
+
+            return Expression.Lambda<Func<TEntity, TProperty>>(body, parameters).Compile();
+        }
 
         public static MemberInfo GetMember(this Expression e)
         {
