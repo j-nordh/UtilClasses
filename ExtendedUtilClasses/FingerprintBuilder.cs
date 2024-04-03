75f8215feb3531fa1a0b0da9b250228074f3ca80
jn-wideco <158475213+jn-wideco@users.noreply.github.com>
2024-04-03 21:42:12 +0200

    Update FingerprintBuilder.cs
    Improved FingerprintBuilder to ease debugging, better handle null and simplify (hopefully) member expression handling and compilation

--- ExtendedUtilClasses/FingerprintBuilder.cs
+++ ExtendedUtilClasses/FingerprintBuilder.cs
@@ -5,6 +5,7 @@ using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Net;
 using System.Security.Cryptography;
 using System.Text;
 using ExtendedUtilClasses.Extensions.DateTimes;
@@ -25,17 +26,43 @@ namespace ExtendedUtilClasses;
  */
 public class FingerprintBuilder<T>
 {
-    private readonly SortedDictionary<string, Func<T, string>> _valueSelectors = new(StringComparer.OrdinalIgnoreCase);
+    private class Extractor
+    {
+        private readonly Func<T, string> _f;
+
+        public string Name { get; }
+
+        public Extractor(string prop, Func<T, string> f)
+        {
+            _f = f;
+            Name = prop;
+        }
+
+        public string Run(T? obj)
+        {
+            if (null == obj) return "";
+            var ret = _f(obj);
+            return ret;
+        }
+    }
+
+    private readonly List<Extractor> _valueSelectors = new();
     public HashAlgorithm HashAlgorithm { get; set; } = new HMACSHA256();
+
+    #region Adds
 
     public FingerprintBuilder<T> Add(Expression<Func<T, int>> me) => AddToString(me);
     public FingerprintBuilder<T> Add(Expression<Func<T, long>> me) => AddToString(me);
+    public FingerprintBuilder<T> AddNullable(Expression<Func<T, long?>> me) => AddToString(me);
 
     public FingerprintBuilder<T> Add(Expression<Func<T, float>> me) =>
         Add(me, f => f.ToString(CultureInfo.InvariantCulture));
 
+    public FingerprintBuilder<T> Add_Nullable(Expression<Func<T, float?>> me) =>
+        Add(me, f => f?.ToString(CultureInfo.InvariantCulture) ?? "");
+
     public FingerprintBuilder<T> Add<T2>(Expression<Func<T, T2>> me, Fingerprinter<T2> fp) =>
-        Add(me, o => fp.Get(o));
+        Add(me, fp.Get);
 
     public FingerprintBuilder<T> Add(Expression<Func<T, double>> me) =>
         Add(me, f => f.ToString(CultureInfo.InvariantCulture));
@@ -43,40 +70,66 @@ public class FingerprintBuilder<T>
     public FingerprintBuilder<T> Add(Expression<Func<T, DateTime>> me) =>
         Add(me, f => f.ToSaneString());
 
+    public FingerprintBuilder<T> Add_Nullable(Expression<Func<T, DateTime?>> me) =>
+        Add(me, f => f.ToSaneString());
+
+    public FingerprintBuilder<T> Add(Expression<Func<T, DateTime?>> me) =>
+        Add(me, f => f.ToSaneString());
+
+    public FingerprintBuilder<T> Add(Expression<Func<T, byte[]>> me) =>
+        Add(me, f => f.ToHexString());
+
+    public FingerprintBuilder<T> Add_Nullable(Expression<Func<T, byte[]?>> me) =>
+        Add(me, f => f.ToHexString());
+
     public FingerprintBuilder<T> Add(Expression<Func<T, DateOnly>> me) =>
         Add(me, f => f.ToSaneString());
+
+    public FingerprintBuilder<T> Add_Nullable(Expression<Func<T, DateOnly?>> me) =>
+        Add(me, f => f?.ToSaneString() ?? "");
 
     public FingerprintBuilder<T> Add(Expression<Func<T, TimeOnly>> me) =>
         Add(me, f => f.ToSaneString());
 
+    public FingerprintBuilder<T> Add_Nullable(Expression<Func<T, TimeOnly?>> me) =>
+        Add(me, f => f?.ToSaneString() ?? "");
+
     public FingerprintBuilder<T> Add(Expression<Func<T, string>> me) =>
         Add(me, s => s);
+
+    public FingerprintBuilder<T> Add_Nullable(Expression<Func<T, string?>> me) =>
+        Add(me, s => s ?? "");
+
+    public FingerprintBuilder<T> Add_Nullable(Expression<Func<T, int?>> me) =>
+        AddToString(me);
 
     public FingerprintBuilder<T> Add(Expression<Func<T, bool>> me) =>
         Add(me, s => s ? "true" : "false");
 
     public FingerprintBuilder<T> Add(Expression<Func<T, Guid>> me) => AddToString(me);
+    public FingerprintBuilder<T> Add_Nullable(Expression<Func<T, Guid?>> me) => AddToString(me);
 
     private FingerprintBuilder<T> AddToString<TObj>(Expression<Func<T, TObj>> me) =>
-        Add(me, o => o.ToString());
+        Add(me, o => o?.ToString() ?? "");
 
     public FingerprintBuilder<T> Add<TInput>(Expression<Func<T, TInput>> getMemberExpression,
-        Expression<Func<TInput, string>> transformValueExpression)
+        Func<TInput, string> transform)
     {
         if (getMemberExpression == null) throw new ArgumentNullException(nameof(getMemberExpression));
-        if (transformValueExpression == null) throw new ArgumentNullException(nameof(transformValueExpression));
         if (getMemberExpression.Body is not MemberExpression memberExpression)
             throw new ArgumentException("Expression must be a member expression.");
-        if (_valueSelectors.ContainsKey(memberExpression.Member.Name))
-            throw new ArgumentException($"Member '{memberExpression.Member.Name}' has already been added.");
+        var name = memberExpression.Member.Name;
+        if (_valueSelectors.Any(e => e.Name.EqualsOic(name)))
+            throw new ArgumentException($"Member '{name}' has already been added.");
 
         var getValue = getMemberExpression.Compile();
-        var transform = transformValueExpression.Compile();
 
-        _valueSelectors[memberExpression.Member.Name] = obj => transform(getValue(obj));
+        _valueSelectors.Add(new(name, o => transform(getValue(o))));
 
         return this;
     }
+
+    #endregion
 
     public FingerprintBuilder<T> With(HashAlgorithm ha)
     {
@@ -91,7 +144,7 @@ public class FingerprintBuilder<T>
 
         return new Fingerprinter<T>(obj =>
                 _valueSelectors
-                    .Select(item => item.Value(obj))
+                    .Select(item => item.Run(obj))
                     .NotNull()
                     .ComputeHash(HashAlgorithm)
                     .ToHexString(),
@@ -100,7 +153,8 @@ public class FingerprintBuilder<T>
 }
 
 public interface IFingerPrinter;
-public class Fingerprinter<T> :IFingerPrinter
+
+public class Fingerprinter<T> : IFingerPrinter
 {
     private readonly Func<T?, string> _single;
     private readonly Func<IEnumerable<T>?, string> _multi;
@@ -108,10 +162,10 @@ public class Fingerprinter<T> :IFingerPrinter
     public Fingerprinter(Func<T?, string> single, HashAlgorithm hashAlgorithm)
     {
         _single = o => null == o ? "" : single(o);
-        _multi = os => os
+        _multi = os => os?
             .Select(_single)
             .ComputeHash(hashAlgorithm)
-            .ToHexString();
+            .ToHexString() ?? "";
     }
 
     public string Get(T? obj) => _single(obj);
@@ -133,6 +187,7 @@ public static class FingerprintBuilder
             .NotNull()
             .Select(Encoding.UTF8.GetBytes)
             .Aggregate(memory, (stream, bs) => stream.Write(bs));
+        memory.Position = 0;
         return ha.ComputeHash(memory);
     }
 }
